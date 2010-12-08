d2839e5665ed95babc50701cad6ec1f563997309
Nano Taboada <nanotaboada@msn.com>
2010-12-08 18:32:27 -0300

    Adding Advanced Encryption Standard (Rijndael) sample project.

---
+++ Dotnet.Samples.Rijndael/Dotnet.Samples.Rijndael/src/Aes.cs
@@ -0,0 +1,76 @@
+﻿#region License
+// Copyright (c) 2010 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+#endregion
+
+namespace Dotnet.Samples.Rijndael
+{
+    #region References
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+    using System.Text;
+    #endregion
+
+    public class Aes
+    {
+        #region Encryption
+        public string Encrypt(string plaintext, string passphrase, string salt, string hashAlgorithm, int iterations, string vector, int keySize)
+        {
+            byte[] vectorData = Encoding.ASCII.GetBytes(vector);
+            byte[] saltData = Encoding.ASCII.GetBytes(salt);
+            byte[] plaintextData = Encoding.UTF8.GetBytes(plaintext);
+            PasswordDeriveBytes derivedPassword = new PasswordDeriveBytes(passphrase, saltData, hashAlgorithm, iterations);
+            byte[] keyData = derivedPassword.GetBytes(keySize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyData, vectorData);
+            MemoryStream persistence = new MemoryStream();
+            CryptoStream transformer = new CryptoStream(persistence, encryptor, CryptoStreamMode.Write);
+            transformer.Write(plaintextData, 0, plaintextData.Length);
+            transformer.FlushFinalBlock();
+            byte[] ciphertextData = persistence.ToArray();
+            persistence.Close();
+            transformer.Close();
+            return Convert.ToBase64String(ciphertextData);
+        }
+        #endregion
+        #region Decryption
+        public string Decrypt(string ciphertext, string passphrase, string salt, string hashAlgorithm, int iterations, string vector, int keySize)
+        {
+            byte[] vectorData = Encoding.ASCII.GetBytes(vector);
+            byte[] saltData = Encoding.ASCII.GetBytes(salt);
+            byte[] cyphertextData = Convert.FromBase64String(ciphertext);
+            PasswordDeriveBytes derivedPassword = new PasswordDeriveBytes(passphrase, saltData, hashAlgorithm, iterations);
+            byte[] keyData = derivedPassword.GetBytes(keySize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyData, vectorData);
+            MemoryStream persistence = new MemoryStream(cyphertextData);
+            CryptoStream transformer = new CryptoStream(persistence, decryptor, CryptoStreamMode.Read);
+            byte[] plaintextData = new byte[cyphertextData.Length];
+            int plaintextSize = transformer.Read(plaintextData, 0, plaintextData.Length);
+            persistence.Close();
+            transformer.Close();
+            return Encoding.UTF8.GetString(plaintextData, 0, plaintextSize);
+        }
+        #endregion
+    }
+}
---
+++ Dotnet.Samples.Rijndael/Dotnet.Samples.Rijndael/src/Win.xaml.cs
@@ -0,0 +1,120 @@
+﻿#region License
+// Copyright (c) 2010 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+#endregion
+
+namespace Dotnet.Samples.Rijndael
+{
+    #region References
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    #endregion
+
+    /// <summary>
+    /// Interaction logic for Win.xaml
+    /// </summary>
+    public partial class Win : Window
+    {
+        public Win()
+        {
+            this.InitializeComponent();
+            this.InitializeTextBoxes();
+        }
+
+        private void InitializeTextBoxes()
+        {
+            this.OutputTextBox.IsReadOnly = true;
+            this.PassphraseTextBox.Text = "foobar";
+            this.SaltTextBox.Text = "NaCl";
+            this.HashAlgorithmTextBox.Text = "SHA1";
+            this.IterationsTextBox.Text = "1";
+            this.VectorTextBox.Text = "0110111001110100";
+            this.KeySizeTextBox.Text = "128";
+
+        }
+
+        private void EncryptDecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this.EncryptDecryptButton.Content.Equals("Encrypt"))
+                {
+                    this.SetTextBoxesReadOnly(true);
+
+                    Aes aes = new Aes();
+
+                    this.OutputTextBox.Text = aes.Encrypt(this.InputTextBox.Text,
+                                                          this.PassphraseTextBox.Text,
+                                                          this.SaltTextBox.Text,
+                                                          this.HashAlgorithmTextBox.Text,
+                                                          Convert.ToInt32(this.IterationsTextBox.Text),
+                                                          this.VectorTextBox.Text,
+                                                          Convert.ToInt32(this.KeySizeTextBox.Text)
+                                                          );
+
+                    this.EncryptDecryptButton.Content = "Decrypt";
+                }
+                else
+                {
+                    Aes aes = new Aes();
+
+                    this.OutputTextBox.Text = aes.Decrypt(this.OutputTextBox.Text,
+                                                            this.PassphraseTextBox.Text,
+                                                            this.SaltTextBox.Text,
+                                                            this.HashAlgorithmTextBox.Text,
+                                                            Convert.ToInt32(this.IterationsTextBox.Text),
+                                                            this.VectorTextBox.Text,
+                                                            Convert.ToInt32(this.KeySizeTextBox.Text)
+                                                            );
+
+                    this.EncryptDecryptButton.Content = "Encrypt";
+
+                    this.SetTextBoxesReadOnly(false);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Exception caught: " + error.Message);
+            }
+        }
+
+        private void SetTextBoxesReadOnly(bool value)
+        {
+            this.InputTextBox.IsReadOnly = value;
+            this.PassphraseTextBox.IsReadOnly = value;
+            this.SaltTextBox.IsReadOnly = value;
+            this.HashAlgorithmTextBox.IsReadOnly = value;
+            this.IterationsTextBox.IsReadOnly = value;
+            this.VectorTextBox.IsReadOnly = value;
+            this.KeySizeTextBox.IsReadOnly = value;
+        }
+
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.OutputTextBox.Text = String.Empty;
+        }
+
+        private void AesWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
